315263b47a051f74dd23fcb8953e79305f765ffc
ani <animesh.ani@live.com>
2020-08-22 23:46:05 -0400

    ani

--- av_create_dataset.py
+++ av_create_dataset.py
@@ -19,6 +19,10 @@ Example:
 from rc_data_handler import DataHandler
 
 
+#___Global Variables:
+TYPE = ['random', 'folded', 'controlled']
+
+
 #___Main Method:
 def main():
     """This is the Main Method.
@@ -29,26 +33,46 @@ def main():
 
     datahandler = DataHandler()
 
-    # # merge csv files in a folder
-    # datahandler.merge_all("data/lists/refined/0_6Floor/", "data/lists/refined/0_6Floor.csv")
+    # # merge all csv files in a folder to one csv file
+    # datahandler.merge_all("data/lists/Base Dataset/0_2Floor/", 
+    #                       "data/lists/Refined/0_2Floor.csv")
+    # datahandler.merge_all("data/lists/Base Dataset/0_6Floor/", 
+    #                       "data/lists/Refined/0_6Floor.csv")
+    # datahandler.merge_all("data/lists/Base Dataset/1_2Floor/", 
+    #                       "data/lists/Refined/1_2Floor.csv")
 
     # # remove data when car is not running
-    # datahandler.refine_running("data/lists/refined.csv", "data/lists/running.csv")
-
-    # # plot histogram for servo and motor data from total dataset
-    # datahandler.histogram("data/train.csv", "")
+    # datahandler.refine_running("data/lists/Refined/0_2Floor.csv", 
+    #                             "data/lists/Running/0_2Floor.csv", 15)
+    # datahandler.refine_running("data/lists/Refined/0_6Floor.csv", 
+    #                             "data/lists/Running/0_6Floor.csv", 15)
+    # datahandler.refine_running("data/lists/Refined/1_2Floor.csv", 
+    #                             "data/lists/Running/1_2Floor.csv", 15)
 
     # # devide dataset according to servo values
-    # datahandler.devide_data("data/lists/running.csv", "data/lists/")
+    # datahandler.merge_all("data/lists/Running/", 
+    #                       "data/lists/Servo Based Devided/running.csv")
+    # datahandler.devide_data("data/lists/Servo Based Devided/running.csv", 
+    #                         "data/lists/Servo Based Devided/")
 
-    # # create train, test, dev dataset
+    # # create random sorted train, test, dev dataset
+    # ratio = [0.8,0.1,0.1] # train:test:dev
+    # datahandler.train_test_dev(TYPE[0], "data/lists/Running/",
+    #                             "data/lists/Random/", ratio)
+    
+    # # create 5-fold cross validation train, test, dev dataset
+    # datahandler.train_test_dev(TYPE[1], "data/lists/Running/",
+    #                             "data/lists/5 Fold Cross-Val/")
+
+    # # create train, test, dev dataset from servo based devided dataset
     # # total = [3161,5651,11678,8855,5987,11707,3736,6934,5241,1763,1340]
     # total = [1340,1340,1340,1340,1340,1340,1340,1340,1340,1340,1340]
-    # ratio = [0.8,0.1,0.1]
-    # datahandler.train_test_dev(total, ratio)
+    # ratio = [0.8,0.1,0.1] # train:test:dev
+    # datahandler.train_test_dev(TYPE[2], "data/lists/Servo Based Devided/",
+    #                             "data/lists/Controlled/", ratio, total)
 
-    # plot histogram for servo and motor data from train dataset
-    datahandler.histogram("data/lists/train.csv", "output/curves/")
+    # plot histogram
+    datahandler.histogram("data/train.csv", "data/histogram/")
 
     return None
 
--- rc_data_handler.py
+++ rc_data_handler.py
@@ -1,70 +1,64 @@
 #!/usr/bin/env python
-#
-#==============================================================================
-# Initialization
-#==============================================================================
-# revision history
-#  20200511 (Animesh): baseline software
-#
-# usage: from rc_data_handler import DataHandler
-#
-# This script refines dataset and explores prediction
-#
-#==============================================================================
-# Import Modules
-#==============================================================================
-#
-# import global modules
-#
+# -*- coding: utf-8 -*-
+
+"""Race-car Data Creation Class.
+
+This script contains all utilities to create proper dataset. 
+
+Revision History:
+        2020-05-10 (Animesh): Baseline Software.
+        2020-08-22 (Animesh): Updated Docstring.
+
+Example:
+        from rc_data_handler import DataHandler
+
+"""
+
+
+#___Import Modules:
 import os
+import random
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# import local modules
-#
 from rc_nn_utility import ParseData
 
-#==============================================================================
-# Global Variables
-#==============================================================================
+
+#___Global Variables:
 SEED = 717
 
-#==============================================================================
-# Classes
-#==============================================================================
 
-# class: DataHandler
-#
-# This class refines dataset and explores prefdiction
-#
+#__Classes:
 class DataHandler:
+    """Data Creation Utility Class.
+    
+    This class contains all methods to complete create datasets such as random
+    data set, or 5 fold cross validation dataset.
+    
+    """
 
-    #==========================================================================
-    # method: constructor
-    #
-    # arguments: none
-    #
-    # return: none
-    #
+
     def __init__(self):
+        """Constructor.
+        
+
+        """
         
         pass
-        
-    #
-    # end of method
+    
 
-    #==========================================================================
-    # method: merge_all
-    #
-    # arguments:
-    #  idir: directory path containing all csv lists
-    #  output: output list
-    #
-    # return: none
-    #
-    # This method merges contents from multiple csv files
-    #
     def merge_all(self, idir, output):
+        """File Merger.
+        
+        This method merges contents from multiple csv files.
+        
+        Args:
+            idir (directory path): Directory path containing all csv files.
+            output (csv file): File containing all contents.
+        
+        Returns:
+            (float): Accuracy percentage.
+
+        """
         
         # read all files from provided folder
         files = os.listdir(idir)
@@ -73,28 +67,48 @@ class DataHandler:
         for ifile in files:
             
             # collect contents from files in provided folder
-            content.extend(pd.read_csv(os.path.join(idir, \
+            if ifile[-4:] == ".csv":
+                content.extend(pd.read_csv(os.path.join(idir, \
                                                     ifile))['image'].to_list())
         
         # write merged contents to output file
         pd.DataFrame(content, columns =['image']).to_csv(output, index=False)
         
         return None
-    #
-    # end of method
     
-    #==========================================================================
-    # method: refine_running
-    #
-    # arguments:
-    #  input: input list
-    #  output: output list
-    #
-    # return: none
-    #
-    # This method removes data when car is not running
-    #
-    def refine_running(self, input, output):
+    
+    def list_merge(self, lists):
+        """List Merger.
+        
+        This method merges contents from multiple lists.
+        
+        Args:
+            lists (list): List of multiple lists to merge.
+        
+        Returns:
+            data (list): Merged list.
+
+        """
+
+        # loop over lists and put them all in one list
+        data = []
+        for list in lists:
+            data.extend(list)
+        
+        return data
+    
+    
+    def refine_running(self, input, output, speed = 15):
+        """Refine Running.
+        
+        This method removes data with provided motor value from a list.
+        
+        Args:
+            input (csv file): File containing contents to refine.
+            output (csv file): File containing refined contents.
+            speed (int): Motor value to be removed.
+
+        """
         
         parsedata = ParseData()
         
@@ -108,28 +122,26 @@ class DataHandler:
             _,_,mot = parsedata.parse_data(data["image"][index])
             
             # append data if car is runneing
-            if mot != 15:
+            if mot != speed:
                 file.append(data["image"][index])
         
         # write merged contents to output file
         pd.DataFrame(file, columns=["image"]).to_csv(output, index=False)
         
         return None
-    #
-    # end of method
     
-    #==========================================================================
-    # method: histogram
-    #
-    # arguments:
-    #  ilist: image file list
-    #  odir: output directory
-    #
-    # return: none
-    #
-    # This method removes data when car is not running
-    #
+    
     def histogram(self, ilist, odir):
+        """Plot Histogram.
+        
+        This method plots histogram from servo and motor value parsed from a 
+        list of images.
+        
+        Args:
+            ilist (csv file): File containing list of images.
+            odir (directory path): Output directory.
+
+        """
         
         parsedata = ParseData()
 
@@ -158,21 +170,18 @@ class DataHandler:
         plt.savefig(os.path.join(odir,"Motor Data Histogram.png"))
         
         return None
-    #
-    # end of method
+
     
-    #==========================================================================
-    # method: devide_data
-    #
-    # arguments:
-    #  ilist: image file list
-    #  odir: output directory
-    #
-    # return: none
-    #
-    # This method devides dataset according to servo value
-    #
     def devide_data(self, ilist, odir):
+        """Dataset Devider.
+        
+        This method devides dataset according to servo value.
+        
+        Args:
+            ilist (csv file): File containing list of images.
+            odir (directory path): Output directory.
+
+        """
         
         parsedata = ParseData()
         
@@ -244,47 +253,224 @@ class DataHandler:
                                                   "servo_20.csv"), index=False)
 
         return None
-    #
-    # end of method
     
-    #==========================================================================
-    # method: train_test_dev
-    #
-    # arguments:
-    #  ilist: image file list
-    #  odir: output directory
-    #
-    # return: none
-    #
-    # This method creates train, test and dev dataset
-    #
-    def train_test_dev(self, total, ratio):
+    
+    def train_test_dev(self, type, idir, odir, ratio=None, total=None):
+        """Final Dataset Creator.
+        
+        This method creates train, test and dev dataset.
+        
+        Args:
+            type (string): Determines the type of input dataset
+            idir (directory path): Directory containing input CSV files.
+            odir (directory path): Output directory.
+            ratio (list): List containing ratio of train, test and dev dataset.
+            total (list): List containing the number of total data to be parsed
+                          from each CSV file.
+
+        """
+        
+        if type == "random":
+            self.random(idir, odir, ratio)
+        elif type == "folded":
+            self.folded(idir, odir)
+        elif type == "controlled":
+            self.controlled(idir, odir, ratio, total)
+
+        return None
+
+
+    def random(self, idir, odir, ratio):
+        """Randomly Shuffled Dataset Creator.
+        
+        This method creates a randomly shuffled train, test and dev dataset.
+        
+        Args:
+            idir (directory path): Directory containing input CSV files.
+            odir (directory path): Output directory.
+            ratio (list): List containing ratio of train, test and dev dataset.
+
+        """
+
+        # read all files from provided folder
+        files = os.listdir(idir)
+        content = []
+
+        for ifile in files:
+
+            # collect contents from files in provided folder
+            if ifile[-4:] == ".csv":
+                content.extend(pd.read_csv(os.path.join(idir, \
+                                                    ifile))['image'].to_list())
+
+        # randomly shuffle dataset
+        random.shuffle(content)
+
+        # devide dataset into train, test, dev set according to given ratio
+        train = content[0:int(ratio[0]*len(content))]
+        test = content[int(ratio[0]*len(content)):
+                       int((ratio[0]+ratio[1])*len(content))]
+        dev = content[int((ratio[0]+ratio[1])*len(content)):]
+
+        # write data
+        pd.DataFrame(train, columns=["image"]).to_csv(odir + 'train.csv',
+                                                      index=False)
+        pd.DataFrame(test, columns=["image"]).to_csv(odir + 'test.csv',
+                                                     index=False)
+        pd.DataFrame(dev, columns=["image"]).to_csv(odir + 'dev.csv',
+                                                    index=False)
+
+        return None
+    
+    
+    def folded(self, idir, odir):
+        """5 Fold Cross-Validation Dataset Creator.
+        
+        This method creates 5 fold cross validation dataset.
+        
+        Args:
+            idir (directory path): Directory containing input CSV files.
+            odir (directory path): Output directory.
+
+        """
+        
+        # read all files from provided folder
+        files = os.listdir(idir)
+        
+        D10 = []
+        D11 = []
+        D20 = []
+        D21 = []
+        D30 = []
+        D31 = []
+        D40 = []
+        D41 = []
+        D50 = []
+        D51 = []
+        for ifile in files:
+            
+            # collect contents from files in provided folder
+            if ifile[-4:] == ".csv":
+                data = pd.read_csv(idir + ifile)
+                D10.extend(data['image'][0:int(len(data)/10)])
+                D11.extend(data['image'][int(len(data)/10):2*int(len(data)/10)])
+                D20.extend(data['image'][2*int(len(data)/10):3*int(len(data)/10)])
+                D21.extend(data['image'][3*int(len(data)/10):4*int(len(data)/10)])
+                D30.extend(data['image'][4*int(len(data)/10):5*int(len(data)/10)])
+                D31.extend(data['image'][5*int(len(data)/10):6*int(len(data)/10)])
+                D40.extend(data['image'][6*int(len(data)/10):7*int(len(data)/10)])
+                D41.extend(data['image'][7*int(len(data)/10):8*int(len(data)/10)])
+                D50.extend(data['image'][8*int(len(data)/10):9*int(len(data)/10)])
+                D51.extend(data['image'][9*int(len(data)/10):])
+        
+        # create 5 folds of train, test and dev dataset
+        train1 = self.list_merge([D10,D11,D20,D21,D30,D31,D40,D41])
+        train2 = self.list_merge([D20,D21,D30,D31,D40,D41,D50,D51])
+        train3 = self.list_merge([D10,D11,D30,D31,D40,D41,D50,D51])
+        train4 = self.list_merge([D10,D11,D20,D21,D40,D41,D50,D51])
+        train5 = self.list_merge([D10,D11,D20,D21,D30,D31,D50,D51])
+        
+        test1 = D50
+        test2 = D10
+        test3 = D20
+        test4 = D30
+        test5 = D40
+        
+        dev1 = D51
+        dev2 = D11
+        dev3 = D21
+        dev4 = D31
+        dev5 = D41
+        
+        # create required directories
+        if not os.path.exists(odir + 'fold1/'):
+            os.mkdir(odir + 'fold1/')
+        if not os.path.exists(odir + 'fold2/'):
+            os.mkdir(odir + 'fold2/')
+        if not os.path.exists(odir + 'fold3/'):
+            os.mkdir(odir + 'fold3/')
+        if not os.path.exists(odir + 'fold4/'):
+            os.mkdir(odir + 'fold4/')
+        if not os.path.exists(odir + 'fold5/'):
+            os.mkdir(odir + 'fold5/')
+        
+        # write data
+        pd.DataFrame(train1,columns=["image"]).to_csv(odir + 'fold1/train.csv',
+                                                      index=False)
+        pd.DataFrame(train2,columns=["image"]).to_csv(odir + 'fold2/train.csv',
+                                                      index=False)
+        pd.DataFrame(train3,columns=["image"]).to_csv(odir + 'fold3/train.csv',
+                                                      index=False)
+        pd.DataFrame(train4,columns=["image"]).to_csv(odir + 'fold4/train.csv',
+                                                      index=False)
+        pd.DataFrame(train5,columns=["image"]).to_csv(odir + 'fold5/train.csv',
+                                                      index=False)
+        
+        pd.DataFrame(test1,columns=["image"]).to_csv(odir + 'fold1/test.csv',
+                                                     index=False)
+        pd.DataFrame(test2,columns=["image"]).to_csv(odir + 'fold2/test.csv',
+                                                     index=False)
+        pd.DataFrame(test3,columns=["image"]).to_csv(odir + 'fold3/test.csv',
+                                                     index=False)
+        pd.DataFrame(test4,columns=["image"]).to_csv(odir + 'fold4/test.csv',
+                                                     index=False)
+        pd.DataFrame(test5,columns=["image"]).to_csv(odir + 'fold5/test.csv',
+                                                     index=False)
+        
+        pd.DataFrame(dev1,columns=["image"]).to_csv(odir + 'fold1/dev.csv',
+                                                    index=False)
+        pd.DataFrame(dev2,columns=["image"]).to_csv(odir + 'fold2/dev.csv',
+                                                    index=False)
+        pd.DataFrame(dev3,columns=["image"]).to_csv(odir + 'fold3/dev.csv',
+                                                    index=False)
+        pd.DataFrame(dev4,columns=["image"]).to_csv(odir + 'fold4/dev.csv',
+                                                    index=False)
+        pd.DataFrame(dev5,columns=["image"]).to_csv(odir + 'fold5/dev.csv',
+                                                    index=False)
+
+        return None
+
+
+    def controlled(self, idir, odir, ratio, total):
+        """Controlled Dataset Creator.
+        
+        This method creates train, test and dev dataset from a servo based
+        devided dataset in a controlled way.
+        
+        Args:
+            idir (directory path): Directory containing input CSV files.
+            odir (directory path): Output directory.
+            ratio (list): List containing ratio of train, test and dev dataset.
+            total (list): List containing the number of total data to be parsed
+                          from each CSV file.
+
+        """
         
         train = [int(x*ratio[0]) for x in total]
         test = [int(x*(ratio[0]+ratio[1])) for x in total]
         total = [int(x) for x in total]
         
-        data_10 = pd.read_csv("data/lists/servo_10.csv").sample(n=total[0],\
+        data_10 = pd.read_csv(idir + "servo_10.csv").sample(n=total[0],\
                                                              random_state=SEED)
-        data_11 = pd.read_csv("data/lists/servo_11.csv").sample(n=total[1],\
+        data_11 = pd.read_csv(idir + "servo_11.csv").sample(n=total[1],\
                                                              random_state=SEED)
-        data_12 = pd.read_csv("data/lists/servo_12.csv").sample(n=total[2],\
+        data_12 = pd.read_csv(idir + "servo_12.csv").sample(n=total[2],\
                                                              random_state=SEED)
-        data_13 = pd.read_csv("data/lists/servo_13.csv").sample(n=total[3],\
+        data_13 = pd.read_csv(idir + "servo_13.csv").sample(n=total[3],\
                                                              random_state=SEED)
-        data_14 = pd.read_csv("data/lists/servo_14.csv").sample(n=total[4],\
+        data_14 = pd.read_csv(idir + "servo_14.csv").sample(n=total[4],\
                                                              random_state=SEED)
-        data_15 = pd.read_csv("data/lists/servo_15.csv").sample(n=total[5],\
+        data_15 = pd.read_csv(idir + "servo_15.csv").sample(n=total[5],\
                                                              random_state=SEED)
-        data_16 = pd.read_csv("data/lists/servo_16.csv").sample(n=total[6],\
+        data_16 = pd.read_csv(idir + "servo_16.csv").sample(n=total[6],\
                                                              random_state=SEED)
-        data_17 = pd.read_csv("data/lists/servo_17.csv").sample(n=total[7],\
+        data_17 = pd.read_csv(idir + "servo_17.csv").sample(n=total[7],\
                                                              random_state=SEED)
-        data_18 = pd.read_csv("data/lists/servo_18.csv").sample(n=total[8],\
+        data_18 = pd.read_csv(idir + "servo_18.csv").sample(n=total[8],\
                                                              random_state=SEED)
-        data_19 = pd.read_csv("data/lists/servo_19.csv").sample(n=total[9],\
+        data_19 = pd.read_csv(idir + "servo_19.csv").sample(n=total[9],\
                                                              random_state=SEED)
-        data_20 = pd.read_csv("data/lists/servo_20.csv").sample(n=total[10],\
+        data_20 = pd.read_csv(idir + "servo_20.csv").sample(n=total[10],\
                                                              random_state=SEED)
         
         data_train = pd.DataFrame([], columns=["image"])
@@ -360,27 +546,14 @@ class DataHandler:
         data_dev = data_dev.append(data_20[test[10]:total[10]], \
                                                    ignore_index = True)
         
-        data_train.to_csv("data/lists/train.csv", index=False)
-        data_test.to_csv("data/lists/test.csv", index=False)
-        data_dev.to_csv("data/lists/dev.csv", index=False)
+        # write data
+        data_train.to_csv(odir + "train.csv", index=False)
+        data_test.to_csv(odir + "test.csv", index=False)
+        data_dev.to_csv(odir + "dev.csv", index=False)
         
         return None
-    #
-    # end of method
-
-#==============================================================================
 
 
-
-
-
-
-
-#==============================================================================
-# Debugging Block ANI717
-#==============================================================================
-# datahandler = DataHandler()
-# datahandler.merge_all("data/lists/refined/", "data/lists/refined.csv")
-
-# datahandler = DataHandler()
-# datahandler.refine_running("data/lists/refined.csv", "data/lists/running.csv")
+#                                                                              
+# end of file
+"""ANI717"""
